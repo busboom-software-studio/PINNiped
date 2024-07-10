8b1124e553f6595c2f008ac51ffb16a943e2e7c7
Eric Busboom <eric@busboom.org>
2024-07-10 07:29:58 -0700

    Added dtheta

--- notebooks/examples/Trajectory.py
+++ notebooks/examples/Trajectory.py
@@ -38,12 +38,11 @@ class Constants:
     drag_f: callable
     g: float = 9.81       # Gravitational acceleration (m/s^2)
     dt: float = 0.01      # Time step (s)
-    C_d: float = 0.047    # Drag coefficient
     rho: float = 1.225    # Air density (kg/m^3)
+    C_d: float = 0.047    # Drag coefficient
     A: float = 0.045      # Cross-sectional area (m^2)
     m: float = 0.145      # Mass (kg)
     
-
 
 # Event function to stop integration when y < 0
 def event_y_below_zero(t, state, consts: Constants ):
@@ -56,6 +55,8 @@ event_y_below_zero.direction = -1 # When y == 0, look for transition from pos to
 def drag(t, vx, vy):
     """Compute the drag force"""
     v = np.sqrt(vx**2 + vy**2) # Magnitude of velocity vector
+    a =  math.atan2(vy, vx)
+    
     F_d = 0.5 * consts.C_d * consts.rho * consts.A * v**2
     
     return F_d
@@ -65,7 +66,7 @@ def _projectile_motion( t, state, consts: Constants):
     """Calculate dy/dt for the state y. This version includes all state
     values, and the input is the same structure as the output. """
     
-    x, y, vx, vy, ax, ay = state
+    x, y, theta, vx, vy, dtheta, ax, ay = state
     
     v = np.sqrt(vx**2 + vy**2) # Magnitude of velocity vector
     a =  math.atan2(vy, vx)
@@ -76,19 +77,20 @@ def _projectile_motion( t, state, consts: Constants):
 
     ay = -consts.g - (F_d * math.sin(a) / consts.m )
     
-    return (ax, ay, vx, vy, ax, ay )
+    dtheta = 0
+    
+    return (x, y, theta, vx, vy, dtheta, ax, ay )
 
 def projectile_motion( t, yn, consts: Constants ):
     """adapt _projectile_motion"""
     
    
-  
-    if len(yn) < 6:
-        yn = np.append(yn, np.array([0,0]))
+    if len(yn) < 8:
+        yn = np.append(yn, np.array([0,0,0]))
     
     yn1 = _projectile_motion(t, yn, consts)
     
-    return yn1[2:]
+    return yn1[3:]
 
 def throw(v0, angle, consts: Constants):
     """Solve a trajectory for an initial velocity and angle"""
@@ -107,22 +109,26 @@ def throw(v0, angle, consts: Constants):
     t_span = (0, t_max)  
     t_eval = np.arange(0, t_max, consts.dt)
 
+
+    y0 = [
+            x0, # x 
+            y0, # y
+            angle_rad, # theta 
+            vx0, # vx 
+            vy0, # vy, 
+          ]
+
     solution = solve_ivp(projectile_motion, t_span, 
-                         y0 =  [x0, y0, vx0, vy0], # Initial State
+                         y0 =  y0, # Initial State
                          t_eval=t_eval, 
                          args = (consts,), events=event_y_below_zero)
 
     t = solution.t
-    x, y, vx, vy = solution.y # Unpack the states
+   
+    x, y, theta, vx, vy  = solution.y # Unpack the states
     
-    # Calculate drag force and accelerations for each time step
-    # We have to re-calc these b/c they are hard to pass out of projectile_motion() through solve_ivp()
-    # However, note that these equations are working with vectors, not scalars like they were in 
-    # projectile_motion()
-
-    # drag_v = np.vectorize(drag)
     
-    F_d = consts.drag_f(t, vx, vy)
+    F_d = np.vectorize(consts.drag_f)(t, vx, vy)
     
     v = np.sqrt(vx**2 + vy**2)
     ax = -F_d * vx / (consts.m * v)
